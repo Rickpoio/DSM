81a4d1bd6077077a69c68d3635064d0f3217206f
Rickpoio <esricardo.ivan03@gmail.com>
2026-05-05 12:54:55 -0600

    Barra de navegacion se ha creado la estructura de la barra de navegacion pero falta corregir bugs visuales relacionados a esta

--- app/src/main/java/com/example/proyectodecatedra/MainActivity.kt
+++ app/src/main/java/com/example/proyectodecatedra/MainActivity.kt
@@ -10,25 +10,61 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
+import android.widget.ImageView
+import android.widget.PopupMenu
+import java.lang.Exception
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+lateinit var bottomNav : BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//
+//        }
+        loadFragment(DashBoardFragment())
+        bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)!!
+        bottomNav.setOnItemSelectedListener {
+           when(it.itemId) {
+               R.id.home -> {
+                   loadFragment(DashBoardFragment())
+                   true
+               }
+               R.id.perfil -> {
+                   loadFragment(PerfilFragment())
+                   true
+               }
+               R.id.noticias -> {
+                   loadFragment(NoticiasFragment())
+                   true
+               }
+               R.id.presupuestos -> {
+                   loadFragment(PresupuestosFragment())
+                   true
+               }
+               R.id.movimientos -> {
+                   loadFragment(MovimientosFragment())
+                   true
+               }
+               else -> false
+           }
         }
-
-
     }
 
-
+    private fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
 
 
 }
