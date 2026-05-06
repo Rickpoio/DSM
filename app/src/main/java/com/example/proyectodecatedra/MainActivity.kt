f0b5baf483c5cb54eb71859ec161f0ce7d910c83
Rickpoio <esricardo.ivan03@gmail.com>
2026-05-05 21:22:06 -0600

    Correcion de estilos se corrigieron estilos de recursos en la barra de navegacion y en los contenedores

--- app/src/main/java/com/example/proyectodecatedra/MainActivity.kt
+++ app/src/main/java/com/example/proyectodecatedra/MainActivity.kt
@@ -25,12 +25,12 @@ lateinit var bottomNav : BottomNavigationView
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//
-//        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right,0)
+            insets
+
+        }
         loadFragment(DashBoardFragment())
         bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)!!
         bottomNav.setOnItemSelectedListener {
