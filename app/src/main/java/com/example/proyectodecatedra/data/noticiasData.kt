3140e37af8593205e1265a35175767c3cd2b4fbd
Rickpoio <esricardo.ivan03@gmail.com>
2026-05-08 14:31:21 -0600

    estructura del segmento de noticias implementacion de un recyclerView para el segmento de noticias

---
+++ app/src/main/java/com/example/proyectodecatedra/data/noticiasData.kt
@@ -0,0 +1,10 @@
+package com.example.proyectodecatedra.data
+
+import java.sql.Date
+
+data class noticiasData(
+    val id: String,
+    val titulo: String,
+    val fecha: String,
+    val categoria: String
+)
